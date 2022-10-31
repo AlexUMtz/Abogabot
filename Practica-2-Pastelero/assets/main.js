c353237caa8161e87c14546ffb73bcda69f0c9a0
Alejandro <alejandro_umartinez@outlook.com>
2022-10-31 16:15:47 -0600

    Se termino la pagina del usuario

---
+++ Practica-2-Pastelero/assets/main.js
@@ -0,0 +1,42 @@
+var swiper = new Swiper(".sabores__slider", {
+    loop: true,
+    spaceBetween: 30,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+
+  var swiper = new Swiper(".adornos__slider", {
+    loop: true,
+    spaceBetween: 30,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 4,
+      },
+    },
+  });
